92601bf1a082149e36d050c594967b097c2b39ea
SANIA <106233951+saniawati021@users.noreply.github.com>
2022-11-04 14:10:43 +0700

    Add files via upload

---
+++ percabangan/tugas3_program_visual/Tugas3_program_visual.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package percabangan.tugas3_program_visual;
+
+/**
+ *
+ * @author User
+ */
+public class Tugas3_program_visual {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
